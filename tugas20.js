0335fe01ac292f1aa5e5d853032d9d65e9c3aad8
Decky Fajar Sidiq <46802206+deckyfajars@users.noreply.github.com>
2021-07-01 17:03:07 +0700

    Add files via upload

---
+++ tugas20.js
@@ -0,0 +1,7 @@
+function cari(){
+  var kata1 = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+  console.log(kata1.match("are"));
+  console.log(kata1.match("words"));
+}
+
+cari();
